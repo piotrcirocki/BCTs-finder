bde5235c3a429b4d3e95363fe20ab648d4c5d6d0
pcirocki <p.cirocki@gmail.com>
2017-07-26 01:26:31 +0200

    bug fixes

--- findDrClusters.R
+++ findDrClusters.R
@@ -1,14 +1,14 @@
 #dodać wiersze jako pojedyncze wpisy - dodać po prostu ID
 #wybierać wiersze, po trzech atrybutach
 
-sink(stdout(), type="message")
+#sink(stdout(), type="message")
 library("Rsamtools")
 getwd()
-setwd("/Users/piotrcirocki/Documents/github/BCT")
+setwd("/home/users/xpcirock/Desktop")
 source("setConfiguration.R")
 source("setAdditionalMethods.R")
 
-setwd("/Users/piotrcirocki/Downloads")
+#setwd("/Users/piotrcirocki/Downloads")
 #library("formatR")
 library("dplyr")
 #tidy_dir(path = ".", recursive = FALSE)
@@ -17,52 +17,63 @@ library("dplyr")
 #> a <- list(a, 5)
 #> a <- list(a, 6)
 
-tresholdValue = 3  #wartosc oznaczająca, próg przy jakim wartość jest zaliczana jako grupa
-countBufferValue = 3  # wartość do jakiej może wystąpić bład w nieprawidlowych readach
+tresholdValue = 2  #wartosc oznaczająca, próg przy jakim wartość jest zaliczana jako grupa
+countBufferValue = 10  # wartość do jakiej może wystąpić bład w nieprawidlowych readach
 bamFilePath = "/Users/piotrcirocki/Downloads/Sample_R_1510_MP5_1791_15_S8.bwa.bam"
 startSearachIndex = 1
 endIndex = 100
 
 browser()
+#debuggingState(on=FALSE)
 #debug(run)
 #run
-startIndex = 1
-endIndex = 10
+
 #configuration <- ConfigurationInitializer()
 #configuration$setConfiguration(bamFilePath)
 #configuration$getConfiguration()
+
 mydf = readRDS(file="data.Rda")
 
 run <- function(tresholdValue , countBufferValue, bamFilePath, startIndex, endIndex) {
-#loading configuration
-    methodHelper <- HelperClass()
-    i = 0
-    
-    for (i in startIndex:endIndex) {
-        row = mydf[i, ]
-        # if 1st row
-        if (i == 1) {
-            methodHelper$setAddNewRow(row)
-        }
-        #if 2nd and further rows
-        if (i != 1) {
-            #find element that already exists in temporary DataFrame
-            relevantElement = methodHelper$getAndFindRelevantElements(as.character(row$rname), as.character(row$rdir), as.character(row$mrnm))
-            #if any in DataFrame
-            if (nrow(relevantElement) > 0) {
-                methodHelper$setAddRowToExistingElement(relevantElement, relevantElement$ID)
-                methodHelper$setCountForWrongRecords(relevantElement$ID)
-                methodHelper$setElementsToNonRelevant(countBufferValue)
-                }
-            else {
-              methodHelper$setAddNewRow(row)
-            }
-        }
+  #loading configuration
+  methodHelper <- HelperClass()
+  i = 0
+  
+  for (i in startIndex:endIndex) {
+    row = mydf[i, ]
+    # if 1st row
+    if (i == 1) {
+      methodHelper$setAddNewRow(row)
     }
-    print(methodHelper$getDfR())
+    #if 2nd and further rows
+    if (i != 1) {
+      #find element that already exists in temporary DataFrame
+      relevantElement = methodHelper$getAndFindRelevantElements(as.character(row$rname), as.character(row$rdir), as.character(row$mrnm))
+      methodHelper$setTreshold(as.character(row$rname), as.character(row$rdir), as.character(row$mrnm))
+      #if any in DataFrame
+      if (nrow(relevantElement) > 0) {
+        
+        methodHelper$setAddRowToExistingElement(relevantElement, relevantElement$ID, row)
+        methodHelper$setCountForWrongRecords(relevantElement$ID)
+        methodHelper$setElementsToNonRelevant(countBufferValue)
+      }
+      else {
+        methodHelper$setAddNewRow(row)
+      }
+    }
+  }
+  
+  print(methodHelper$filterWithTreshold(tresholdValue))
+  #saveRDS(methodHelper$getDfR(), file = "genomeData.Rda")
+  write.csv(methodHelper$getDfR(), file = "genome.csv")
 }
 
 debug(run)
-
+startIndex = 1
+endIndex = 20
 run(tresholdValue , countBufferValue , bamFilePath, startIndex, endIndex)
+#todo dodać nowe informacje do nowych rekordów, nie stare.
+#odsiać tylko te z przekroczonym  treshold 
+#zapisać do pliku
+
 
--- setAdditionalMethods.R
+++ setAdditionalMethods.R
@@ -1,74 +1,88 @@
 HelperClass <- function() {
- 
+  
   thisEnv <- environment()
   
-  dfR <- data.frame(chr = character(), ID = integer(), pos = list(), flag = list(), mrnm = character(), mpos = list(), 
+  dfR <- data.frame(chr = character(), ID = integer(), pos = character(), flag = character(), mrnm = character(), mpos = character(), 
                     countBuffer = integer(), treshold = integer(), isRelevant = logical(), dir = character(), stringsAsFactors = FALSE)
- 
-   mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
-      condition <- eval(substitute(condition), .data, envir)
-      .data[condition, ] <- .data[condition, ] %>% mutate(...)
-      .data
-    }
+  
+  mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
+    condition <- eval(substitute(condition), .data, envir)
+    .data[condition, ] <- .data[condition, ] %>% mutate(...)
+    .data
+  }
   me <- list(thisEnv = thisEnv, 
-        setAddRowToExistingElement = function(mydfRow, searchID) {
-          
-          newRow <- list()
-          newRow$chr = as.character(mydfRow$chr)
-          newRow$ID = searchID
-          newRow$pos <- list(as.character(mydfRow$pos))
-          newRow$flag <- list(as.character(mydfRow$flag))
-          newRow$mrnm = as.character(mydfRow$mrnm)
-          newRow$mpos <- list(as.character(mydfRow$mpos))
-          newRow$countBuffer = 0
-          newRow$countBuffer = as.integer(newRow$countBuffer)
-          newRow$treshold = 1
-          newRow$treshold = as.integer(newRow$treshold)
-          newRow$isRelevant = TRUE
-          newRow$dir <- mydfRow$dir
-          dfR =   dplyr::bind_rows(dfR,newRow)
-          assign("dfR",dfR,thisEnv)
-    }, getAndFindRelevantElements = function(chromName, findDir, mrnrmEl) {
-       dfR %>% filter(isRelevant == TRUE, chr == chromName, dir == findDir , mrnm == mrnrmEl) -> relevantElement
-      return(relevantElement)
-    }, setAddNewRow = function(mydfRow) {
-        newRow <- list()
-        newRow$chr = as.character(mydfRow$rname)
-        newRow$ID = mydfRow$ID
-        newRow$pos <- list(as.character(mydfRow$pos))
-        newRow$flag <- list(as.character(mydfRow$flag))
-        newRow$mrnm = as.character(mydfRow$mrnm)
-        newRow$mpos <- list(as.character(mydfRow$mpos))
-        newRow$countBuffer = 0
-        newRow$countBuffer = as.integer(newRow$countBuffer)
-        newRow$treshold = 1
-        newRow$treshold = as.integer(newRow$treshold)
-        newRow$isRelevant = TRUE
-        newRow$dir <- mydfRow$rdir
-        dfR =   dplyr::bind_rows(dfR,newRow)
-        assign("dfR",dfR,thisEnv)
-        },
-    getDataFrame = function(){
-      return(get("dfR",thisEnv))
-    },
-    # naliczanie do bufora dla rekordów, które się nie pojawiły - nakladanie kary
-    setCountForWrongRecords = function(searchID){
-      dfR %>% mutate_cond((ID != searchID & isRelevant == TRUE), countBuffer = countBuffer +1) -> dfR
-      #dfR <- as.data.frame(dfR)
-      assign("dfR",dfR,thisEnv)
-    },
-    setElementsToNonRelevant = function(countBufferValue){
-      # oznaczenie rekordow , jako is Relevant - False
-      #nie spełniają one już więcej warunku - bufor dla nich został przekroczony
-      dfR %>% mutate_cond((countBuffer > countBufferValue & isRelevant == TRUE), isRelevant = FALSE) -> dfR 
-      #dfR <- as.data.frame(dfR)
-      assign("dfR",dfR,thisEnv)
-    },
-    getDfR = function(){
-      assign("MyDataFrame",dfR,thisEnv)
-      return(get("MyDataFrame",thisEnv))
-    }
-    )
+             setAddRowToExistingElement = function(mydfRow, searchID, row) {
+               
+               newRow <- list()
+               newRow$chr = as.character(mydfRow$chr)
+               newRow$ID = searchID
+               #newRow$pos <- list(as.character(mydfRow$pos))
+               newRow$pos = as.character(row$pos)
+               #newRow$flag <- list(as.character(mydfRow$flag))
+               newRow$flag = as.character(row$flag)
+               newRow$mrnm = as.character(mydfRow$mrnm)
+               newRow$mpos = as.character(row$mpos)
+               #newRow$mpos <- list(as.character(mydfRow$mpos))
+               newRow$countBuffer = 0
+               newRow$countBuffer = as.integer(newRow$countBuffer)
+               newRow$treshold = mydfRow$treshold
+               newRow$isRelevant = TRUE
+               newRow$dir <- mydfRow$dir
+               dfR =   dplyr::bind_rows(dfR,newRow)
+               assign("dfR",dfR,thisEnv)
+             }, getAndFindRelevantElements = function(chromName, findDir, mrnrmEl) {
+               dfR %>% filter(isRelevant == TRUE, chr == chromName, dir == findDir , mrnm == mrnrmEl)   %>%  filter(row_number()==1 ) -> relevantElement
+               
+               #dfR %>% mutate_cond((isRelevant == TRUE & chr == chromName & dir == findDir & mrnm == mrnrmEl), treshold =  treshold +1) -> dfR %>%  filter(row_number()==1 ) -> relevantElement
+               return(relevantElement)
+             }, 
+             setTreshold = function(chromName, findDir, mrnrmEl){
+               dfR %>% mutate_cond((isRelevant == TRUE & chr == chromName & dir == findDir & mrnm == mrnrmEl), treshold =  treshold +1) -> dfR
+               assign("dfR",dfR,thisEnv)
+             },
+             
+             setAddNewRow = function(mydfRow) {
+               newRow <- list()
+               newRow$chr = as.character(mydfRow$rname)
+               newRow$ID = mydfRow$ID
+               newRow$pos <- as.character(mydfRow$pos)
+               newRow$flag <- as.character(mydfRow$flag)
+               newRow$mrnm = as.character(mydfRow$mrnm)
+               newRow$mpos <- as.character(mydfRow$mpos)
+               newRow$countBuffer = 0
+               newRow$countBuffer = as.integer(newRow$countBuffer)
+               newRow$treshold = 1
+               #newRow$treshold = as.integer(newRow$treshold)
+               newRow$isRelevant = TRUE
+               newRow$dir <- mydfRow$rdir
+               dfR =   dplyr::bind_rows(dfR,newRow)
+               assign("dfR",dfR,thisEnv)
+             },
+             getDataFrame = function(){
+               return(get("dfR",thisEnv))
+             },
+             # naliczanie do bufora dla rekordów, które się nie pojawiły - nakladanie kary
+             setCountForWrongRecords = function(searchID){
+               dfR %>% mutate_cond((ID != searchID & isRelevant == TRUE), countBuffer = countBuffer +1) -> dfR
+               #dfR <- as.data.frame(dfR)
+               assign("dfR",dfR,thisEnv)
+             },
+             setElementsToNonRelevant = function(countBufferValue){
+               # oznaczenie rekordow , jako is Relevant - False
+               #nie spełniają one już więcej warunku - bufor dla nich został przekroczony
+               dfR %>% mutate_cond((countBuffer > countBufferValue & isRelevant == TRUE), isRelevant = FALSE) -> dfR 
+               #dfR <- as.data.frame(dfR)
+               assign("dfR",dfR,thisEnv)
+             },
+             filterWithTreshold = function(tresholdValue){
+               dfR %>% filter(treshold >= tresholdValue)-> dfRFiltered
+               return(dfRFiltered)
+             },
+             getDfR = function(){
+               assign("MyDataFrame",dfR,thisEnv)
+               return(get("MyDataFrame",thisEnv))
+             }
+  )
   
   ## Define the value of the list within the current environment.
   assign('this',me,envir=thisEnv)
@@ -76,4 +90,4 @@ HelperClass <- function() {
   ## Set the name for the class
   class(me) <- append(class(me),"HelperClass")
   return(me)
- }
+}
